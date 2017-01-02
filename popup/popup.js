875a20db61685f631a2e38c94074ed7acf6e523d
noraworld <noraworld@mail.com>
2017-01-02 15:48:30 +0900

    :trollface: Delete garbage codes

--- popup/popup.js
+++ popup/popup.js
@@ -2,62 +2,17 @@
 
   'use strict';
 
-  var referrer = document.querySelector('#referrer');
-  // let referrerData = undefined;
+  let referrer = document.querySelector('#referrer');
 
-  // if (document.referrer) {
-  //   referrerData = document.referrer;
-  // }
-  // else {
-  //   referrerData = 'none';
-  // }
-
-  // referrer.textContent = referrerData;
-
-  // chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  //   console.log(response.back);
-  // });
-
-  // var foo = [];
-
-  // chrome.tabs.query({active:true}, function(tab) {
-  //   chrome.tabs.sendMessage(tab[0].id, {text:'bbbbb'}, function(response) {
-  //       console.log(response);
-  //       for (var i = 0; i < response.length; i++) {
-  //         foo.push(response[i]);
-  //       }
-  //    });
-  // });
-
-  // for (var i = 0; i < foo.length; i++) {
-  //   console.log(foo[i]);
-  //   referrer.textContent += foo[i];
-  // }
-
-// chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
-//   chrome.tabs.sendMessage(tabs[0].id, {text: 'aa'}, function(response) {
-//     referrer.textContent = tabs[0].id;
-//     if (response) {
-//       referrer.textContent = response;
-//     }
-//     else {
-//       referrer.textContent = 'none'
-//     }
-//   });
-// });
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
-  chrome.tabs.sendMessage(tab[0].id, {pop: "request from popup"}, function(response) {
-    if (chrome.extension.getBackgroundPage().referrer[tab[0].id] === "") {
-      referrer.textContent = "none";
-    }
-    else {
-      referrer.textContent = chrome.extension.getBackgroundPage().referrer[tab[0].id];
-    }
-    // console.log(chrome.extension.getBackgroundPage().referrer);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, {pop: "request from popup"}, function(response) {
+      if (chrome.extension.getBackgroundPage().referrer[tab[0].id] === "") {
+        referrer.textContent = "none";
+      }
+      else {
+        referrer.textContent = chrome.extension.getBackgroundPage().referrer[tab[0].id];
+      }
+    });
   });
-});
-
-// referrer.textContent = new Date().toString();
 
 })();
--- src/inject.js
+++ src/inject.js
@@ -23,38 +23,4 @@
 
   chrome.runtime.sendMessage({referrer: document.referrer}, function(response) {});
 
-  // function test() {
-  //   chrome.runtime.sendMessage({
-  //     greeting: "hello"
-  //   }, function(response) {
-  //     console.log(response);
-  //   });
-  // }
-  // test();
-
-// chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-//     sendResponse({farewell: 'inject'});
-//     return true;
-// });
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   console.log(request);
-//   var res = 'finish';
-//   sendResponse(res);
-//   return true;
-// });
-
-// var ref = [];
-// ref.push(window.location.href + ': ' + document.referrer);
-// console.log(ref);
-
-// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-//   console.log(msg);
-//     sendResponse(ref);
-// });
-
-  // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  //   sendResponse(document.referrer);
-  // });
-
 })();
