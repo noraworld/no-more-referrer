26a4225e08fe6ab15585e4950876964657303956
noraworld <noraworld@mail.com>
2017-01-02 15:44:02 +0900

    :sparkles: Complete basic options and load option from content scripts

--- options/options.js
+++ options/options.js
@@ -3,34 +3,44 @@
   'use strict';
 
   let excludeURL = [];
+  init();
 
-  $('.exclude').children('.url').focus();
+  function init() {
+    load();
+    $('.exclude').children('.url').focus();
 
-  $('.exclude').on('submit', function(event) {
-    // event.preventDefault();
-    console.log($(this).children('.url').val());
-    saveOptions($(this).children('.url'));
-  });
+    $('.exclude').on('submit', function(event) {
+      event.preventDefault();
+      console.log($(this).children('.url').val());
+      add($(this).children('.url'));
+      location.reload();
+    });
 
-  loadOptions();
-  function loadOptions() {
+    $(document).on('click', '.delete', function() {
+      let index = $(this).parent().children('.index').text() - 1;
+      excludeURL.splice(index, 1);
+      save();
+      location.reload();
+    });
+  }
+
+  function load() {
     chrome.storage.sync.get(function(storage) {
+      console.log(storage);
       excludeURL = storage.excludeURL;
-      console.log(excludeURL);
       for (var i = 0; i < excludeURL.length; i++) {
-        $('body').append('<div>' + excludeURL[i] + '</div>');
+        $('.exclude-url-list').append('<tr><td class="index">' + (i + 1) + '</td><td class="value">' + excludeURL[i] + '</td><td class="delete"><button type="button">Delete</button></td></tr>');
       }
     });
   }
 
-  function saveOptions(url) {
-    excludeURL.push(url.val());
-    chrome.storage.sync.set({
-      excludeURL: excludeURL
-    });
+  function add(url) {
+    excludeURL.unshift(url.val());
+    save();
   }
 
-  let excludeURLs = document.querySelectorAll('.exclude .url');
-  console.log(excludeURLs.length);
+  function save() {
+    chrome.storage.sync.set({excludeURL: excludeURL});
+  }
 
 })();
--- src/inject.js
+++ src/inject.js
@@ -2,11 +2,26 @@
 
   'use strict';
 
-  // let meta     = document.createElement('meta');
-  // meta.name    = "referrer";
-  // meta.content = "no-referrer";
+  chrome.storage.sync.get(function(storage) {
+    let execFlag = true;
+    for (var i = 0; i < storage.excludeURL.length; i++) {
+      if (location.host === storage.excludeURL[i]) {
+        execFlag = false;
+      }
+    }
+    if (execFlag) {
+      disableReferrer();
+    }
+  });
 
-  // document.head.appendChild(meta);
+  function disableReferrer() {
+    let meta     = document.createElement('meta');
+    meta.name    = "referrer";
+    meta.content = "no-referrer";
+    document.head.appendChild(meta);
+  }
+
+  chrome.runtime.sendMessage({referrer: document.referrer}, function(response) {});
 
   // function test() {
   //   chrome.runtime.sendMessage({
@@ -37,8 +52,6 @@
 //   console.log(msg);
 //     sendResponse(ref);
 // });
-
-  chrome.runtime.sendMessage({referrer: document.referrer}, function(response) {});
 
   // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   //   sendResponse(document.referrer);
