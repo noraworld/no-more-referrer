fb3d3a3d45bd82b5861cb2bea8f22b7365e86ab5
noraworld <noraworld@mail.com>
2017-01-04 14:20:55 +0900

    :sparkles: Use regular expression to options

--- src/inject.js
+++ src/inject.js
@@ -5,7 +5,7 @@
   chrome.storage.sync.get(function(storage) {
     let execFlag = true;
     for (var i = 0; i < storage.excludeURL.length; i++) {
-      if (location.host === storage.excludeURL[i]) {
+      if (parseURL(location.href).match(RegExp(parseURL('^' + storage.excludeURL[i] + '$')))) {
         execFlag = false;
       }
     }
@@ -19,6 +19,19 @@
     meta.name    = "referrer";
     meta.content = "no-referrer";
     document.head.appendChild(meta);
+  }
+
+  function parseURL(url) {
+    // cut 'http' or 'https'
+    // http://www.example.com  => www.example.com
+    // https://www.example.com => www.example.com
+    url = url.replace(/^https?:\/\//g, '');
+
+    // cut trailing slash
+    // example.com/foo/bar/ => example.com/foo/bar
+    url = (url[url.length-1] === '/') ? url.substr(0, url.length-1) : url;
+
+    return url;
   }
 
   chrome.runtime.sendMessage({referrer: document.referrer}, function(response) {});
