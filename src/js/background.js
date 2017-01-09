a79694d5de125a0cdf04f7a89e90c84bcbb55160
noraworld <noraworld@mail.com>
2017-01-09 20:04:28 +0900

    :recycle: Delete console debug

--- src/js/background.js
+++ src/js/background.js
@@ -3,7 +3,6 @@ var referrer = [];
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
     referrer[tab[0].id] = request.referrer;
-    console.log(tab[0].id);
     sendResponse(referrer);
     if (referrer[tab[0].id] === "") {
       chrome.browserAction.setBadgeText({tabId: tab[0].id, text: "ON"});
